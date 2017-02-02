cd93846d9c2667c3be4234ea0d226936c809e191
森川司 <morikawatsukasa@mba.local>
2017-02-02 22:20:26 +0900

    kadai7

---
+++ 07/select2.php
@@ -0,0 +1,63 @@
+<?php
+//1.  DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('データベースに接続できませんでした。'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_bm_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //$view .= $view.$res..と同じ意味 データを加えていく(.=)
+  //$view .= $res["id"].$res["name"].$res["email"];
+  while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $view .= '<p>'.$res["book_name"]."**".$res["book_url"].'</p>';
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>フリーアンケート表示</title>
+<link rel="stylesheet" href="css/range.css">
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<style>div{padding: 10px;font-size:16px;}</style>
+</head>
+<body id="main">
+<!-- Head[Start] -->
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+      <a class="navbar-brand" href="index2.php">データ登録</a>
+      </div>
+    </div>
+  </nav>
+</header>
+<!-- Head[End] -->
+
+<!-- Main[Start] -->
+<div>
+    <div class="container jumbotron"><?=$view?></div>
+</div>
+<!-- Main[End] -->
+
+</body>
+</html>
